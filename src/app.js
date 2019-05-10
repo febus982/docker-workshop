d360f804de8caefb63d651b2744ea7aa28fc1215
Federico Busetti <federico.busetti@gousto.co.uk>
2019-05-10 12:44:31 +0100

    Add request logging

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+    console.log(`Received request`);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Gousto!!!\n');
